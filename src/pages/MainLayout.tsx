d830943b3a4af524c25fcbc66ea2da77052196e5
gforgues <gui.forgues@gmail.com>
2024-06-23 17:53:02 -0400

    Added default footer with tabler icons

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,5 +1,6 @@
 import {Outlet} from "react-router-dom";
 import {Header} from "../components/Header/Header.js";
+import {Footer} from "../components/Footer/Footer.tsx";
 
 function Layout() {
     return (
@@ -8,6 +9,7 @@ function Layout() {
             <main>
                 <Outlet/>
             </main>
+            <Footer />
         </>
     );
 
